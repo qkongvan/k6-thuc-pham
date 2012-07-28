0177c5e25db4945cb0109227091dc21d6bf50bfa
huytbt <huytbt@gmail.com>
2012-07-28 17:08:13 +0000

    Commit for update

--- dev/webapp/protected/modules/products/controllers/ShopController.php
+++ dev/webapp/protected/modules/products/controllers/ShopController.php
@@ -3,10 +3,30 @@
 class ShopController extends HController
 {
 	public $layout = '//layouts/main';
-	
+
 	public function actionIndex()
 	{
 		$this->render('index');
+	}
+
+	public function actionResetImage()
+	{
+		$uploadPath = YiiBase::getPathOfAlias('webroot') . '/files/products';
+		@set_time_limit(0);
+		$items = ProductItem::model()->findAll();
+		foreach ($items as $item) {
+			$thumbsPath = $uploadPath;
+			$filename = $item->image;
+			@unlink($thumbsPath . '/thumb_' . $filename);
+			// thumbnails image
+			Yii::import("ext.EPhpThumb.EPhpThumb");
+			$thumb = new EPhpThumb();
+			$thumb->init(); //this is needed
+			$thumb->create($uploadPath . '/' . $filename)
+				  ->adaptiveResize(Yii::app()->getModule('products')->widthThumb, Yii::app()->getModule('products')->heightThumb)
+				  ->save($thumbsPath . '/thumb_' . $filename);
+		}
+		echo 'Done!';
 	}
 
 	// Uncomment the following methods and override them if needed
