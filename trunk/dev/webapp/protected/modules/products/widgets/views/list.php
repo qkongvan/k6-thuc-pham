8c3fd5efa5ae05e278fbec9bf33d1b53f7796756
huytbt@gmail.com <huytbt@gmail.com@08755ec5-1c6c-c600-d419-c166c4b567b3>
2012-07-29 09:52:10 +0000

    Commit for update

--- trunk/dev/webapp/protected/modules/products/widgets/views/list.php
+++ trunk/dev/webapp/protected/modules/products/widgets/views/list.php
@@ -8,7 +8,7 @@
 			<?php $url = Yii::app()->createAbsoluteUrl(HController::getCurrentUrl(), array('action'=>'detail', 'product'=>$item->id)); ?>
 			<a href="<?php echo $url; ?>"><img title="<?php echo CHtml::encode($item->name); ?>" rel="<?php echo $item->id.'|||'.$item->price; ?>" alt="" src="<?php if ($item->image) { echo Yii::app()->createAbsoluteUrl('/') . '/files/products/thumb_' . $item->image; } else { echo $baseScriptUrl . '/no_image.gif'; } ?>" /></a>
 			<h5><?php echo CHtml::link($item->name, $url); ?></h5>
-			<p><?php echo ProductsModule::t('Price'); ?>: <span><?php echo CHtml::encode($item->price); ?></span></p>
+			<p><?php echo ProductsModule::t('Price'); ?>: <span><?php echo CHtml::encode($item->price) . ' ' . CHtml::encode($item->unit); ?></span></p>
 		</li>
 	<?php endforeach; ?>
 	</ul>
